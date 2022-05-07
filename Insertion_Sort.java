e104ea8b881701063fd1ca59fa297da5d286cd27
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2022-05-08 01:11:52 +0530

    Update Insertion_Sort.java

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -1,32 +1,25 @@
 import java.util.*;
 public class Insertion_Sort {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		int n = sc.nextInt();
-		int [] arr = new int [n];
-		for (int i=0; i<n; i++)
+	static void insertionSort (int [] arr)
+	{
+		for (int i=0; i<arr.length; i++)
 		{
-			arr[i] = sc.nextInt();
-		}
-		
-		int key, j=0;
-		for (int i=1; i<n; i++)
-		{
-			key = arr[i];
-			j = i-1;
-			while (j>=0 && key<arr[j])
+			for (int j=0; j<i; j++)
 			{
-				arr [j+1] = arr[j];
-				j--;
+				if (arr[j]>arr[i])
+				{
+					int temp = arr [j];
+					arr [j] = arr[i];
+					arr [i] = temp;
+				}
 			}
-			arr [j+1] = key;
 		}
-		
-		for (int element : arr)
-		{
-			System.out.print(element+" ");
-		}
+	}
 
+	public static void main(String[] args) {
+		int [] arr = {5,3,1,6,7};
+		insertionSort (arr);
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
